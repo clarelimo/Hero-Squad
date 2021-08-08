b085e68879e9d1ec8b33e4739eb9a863ae0af8bf
clarelimo <clare.limo@student.moringaschool.com>
2021-08-08 16:03:23 +0300

    list of heroes displayed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import models.Hero;
 import spark.ModelAndView;
+import spark.QueryParamsMap;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
@@ -23,21 +24,26 @@ public class App {
 
         post("/heroes/new",(request, response) -> {
             Map<String,Object> model = new HashMap<String, Object>();
-            model.put("name",request.session().attribute("name"));
-            model.put("age",request.session().attribute("age"));
-            String[] powersAdd = request.session().attribute("power");
+            String name = request.queryParams("name");
+            int age = Integer.parseInt(request.queryParams("age"));
+//            model.put("name",request.session().attribute("name"));
+//            model.put("age",request.session().attribute("age"));
+
+            String[] powersAdd = request.queryMap().toMap().get("power");
             ArrayList<String> powers = new ArrayList<String>();
             for (int i=0;i<powersAdd.length;i++){
                 powers.add(powersAdd[i]);
             }
             model.put("powers",powers);
 
-            String[] weaknessAdd = request.session().attribute("weakness");
+            String[] weaknessAdd =  request.queryMap().toMap().get("weakness");
             ArrayList<String> weaknesses = new ArrayList<String>();
-            for (int i=0;i<weaknessAdd.length;i++){
-                weaknesses.add(weaknessAdd[i]);
+            for (String s:weaknessAdd){
+                weaknesses.add(s);
             }
             model.put("weaknesses",weaknesses);
+
+            Hero hero = new Hero(name,age,powers,weaknesses);
 
            return new ModelAndView(model,"success.hbs");
         }, new HandlebarsTemplateEngine());
